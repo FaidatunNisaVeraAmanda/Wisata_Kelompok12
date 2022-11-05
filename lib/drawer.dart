f8ea788c5d97bf09c2a221f3f97813e12918f157
FaidatunNisaVeraAmanda <faidatun.20001@mhs.unesa.ac.id>
2022-11-05 18:54:26 +0700

    first commit

---
+++ lib/drawer.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MyHeaderDrawer extends StatefulWidget {
+  const MyHeaderDrawer({Key? key}) : super(key: key);
+
+
+  @override
+  _MyHeaderDrawerState createState() => _MyHeaderDrawerState();
+}
+
+class _MyHeaderDrawerState extends State<MyHeaderDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      width: double.infinity,
+      height: 150,
+      padding: const EdgeInsets.only(top: 20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(bottom: 10.0),
+            height: 70.0,
+            decoration: const BoxDecoration(
+              shape: BoxShape.rectangle,
+              image: DecorationImage(
+                image: AssetImage('assets/logo.png'),
+              ),
+            ),
+          ),
+          const Text("Username4U", style: TextStyle(color: Colors.white, fontSize: 15),),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_wisata/drawer.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Center(
+          child: Text("HOME"),
+        ),
+      ),
+
+      drawer: Drawer(
+        backgroundColor: Colors.black,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const MyHeaderDrawer(),
+              ListTile(
+                leading: const Icon(Icons.home, color: Colors.white,),
+                title: const Text("Home", style: TextStyle(color: Colors.white),),
+                onTap: () {Navigator.pop(context);},
+              ),
+              ListTile(
+                leading: const Icon(Icons.person_outline, color: Colors.white,),
+                title: const Text("Profile", style: TextStyle(color: Colors.white),),
+                onTap: () {Navigator.pop(context);},
+              ),
+              ListTile(
+                leading: const Icon(Icons.info_outline_rounded, color: Colors.white,),
+                title: const Text("Information", style: TextStyle(color: Colors.white),),
+                onTap: () {Navigator.pop(context);},
+              ),
+              ListTile(
+                leading: const Icon(Icons.settings, color: Colors.white,),
+                title: const Text("Settings", style: TextStyle(color: Colors.white),),
+                onTap: () {Navigator.pop(context);},
+              ),
+              ListTile(
+                leading: const Icon(Icons.logout, color: Colors.white,),
+                title: const Text("Log Out", style: TextStyle(color: Colors.white),),
+                onTap: () {Navigator.pop(context);},
+              ),
+            ],
+          ),
+        ),
+      ),
+
+      body: Container(
+        color: Colors.green[600],
+        child: GridView.count(
+          
+          padding: const EdgeInsets.all(25),
+          crossAxisCount: 2,
+          children: <Widget>[
+            Card(
+              color: Colors.black,
+              margin: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+              child: Text('New Destination', style: TextStyle(fontSize: 17, fontStyle: FontStyle.italic, color: Colors.white),),
+            ),
+            Card(
+              color: Colors.black,
+              margin: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+              child: InkWell(
+                onTap:() {},
+                child: Column(
+                  children: const <Widget>[
+                    Icon(Icons.garage, size: 100, color: Colors.white,)
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              margin: const EdgeInsets.all(8),
+              child: InkWell(
+                onTap: (){},
+                splashColor: Colors.green[300],
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const <Widget>[
+                      Icon(Icons.widgets_outlined, size: 70, color: Colors.green,),
+                      Text("Surabaya Utara", style: TextStyle(fontSize: 20.0), textAlign: TextAlign.center,),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: const EdgeInsets.all(8),
+              child: InkWell(
+                onTap: (){},
+                splashColor: Colors.green[300],
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const <Widget>[
+                      Icon(Icons.add_home_outlined, size: 70, color: Colors.green,),
+                      Text("Surabaya Timur", style: TextStyle(fontSize: 20.0), textAlign: TextAlign.center,),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: const EdgeInsets.all(8),
+              child: InkWell(
+                onTap: (){},
+                splashColor: Colors.green[300],
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const <Widget>[
+                      Icon(Icons.yard_rounded, size: 70, color: Colors.green,),
+                      Text("Surabaya Barat", style: TextStyle(fontSize: 20.0), textAlign: TextAlign.center,),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: const EdgeInsets.all(8),
+              child: InkWell(
+                onTap: (){},
+                splashColor: Colors.green[300],
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const <Widget>[
+                      Icon(Icons.woo_commerce, size: 70, color: Colors.green,),
+                      Text("Surabaya Selatan", style: TextStyle(fontSize: 20.0), textAlign: TextAlign.center,),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
